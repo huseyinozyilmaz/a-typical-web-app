cdf29186234de95090c4030450105e4a3ba69ad3
Huseyin Ozyilmaz <huseyin@intetech.com>
2015-11-11 16:31:23 +0000

    adds login page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var pause   = require('connect-pause');
 var router  = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +14,22 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
     res.render('layout', {
         title   : 'Login',
-        partial : 'login'
+        partial : 'login',
+        loginFailed: false
+    });
+});
+router.post('/login', pause(1000), function(req, res, next) {
+    var partial = 'login';
+    var loginFailed = true;
+
+    if(req.body.email == 'valid@email.com' && req.body.pwd == 'secret'){
+        loginFailed = false;
+        partial = 'loginsuccess';
+    }
+    res.render('layout', {
+        title   : 'Login',
+        partial : partial,
+        loginFailed: loginFailed
     });
 });
 
