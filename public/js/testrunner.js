7e3d8855a852f53251cbe4c1b7bd123607b50a4b
Huseyin Ozyilmaz <huseyin@intetech.com>
2015-11-16 16:52:41 +0000

    adds trials with test runner

--- public/js/testrunner.js
+++ public/js/testrunner.js
@@ -1,4 +1,5 @@
 $(function(){
+    chai.should();
     var $container = $('.page-body');
     var $iframe = $('#scaled-frame');
     var $window = $(window).on('resize', function(){
@@ -9,38 +10,52 @@ $(function(){
     }).trigger('resize'); //on page load
 
     var tests = [
-    	{
-    		suite:'login',
-    		title:'Verify user can login with valid user account',
-    		verify: function($dom) {
-    			console.log('Runnuning');
-    			console.log($dom);
-    			$dom.find('a[href$="/login"]').trigger('click');
-    		}
-    	}
+        {
+            suite:'login',
+            title:'Verify user can login with valid user account',
+            verify: function($dom) {
+                $dom.find('a[href$="/login"]').trigger('click');
+            }
+        }
     ];
 
     $('#tests').html('');
 
     for (var i = 0; i < tests.length; i++) {
-    	var test = tests[i];
-    	$('#tests').append('<h2 class="test-suite">' + test.suite + '</h2>');
-    	$('#tests').append('<p><span class="test-status"></span>' + test.title + '</p>');
-    	//test.verify($('#scaled-frame').contents());
+        var test = tests[i];
+        $('#tests').append('<h2 class="test-suite">' + test.suite + '</h2>');
+        $('#tests').append('<p><span class="test-status"></span>' + test.title + '</p>');
+        test.verify($('#scaled-frame').contents());
     };
 
-    $("iframe").ready(function () {
-    	console.log('iframe is ready');
-    	console.log($('iframe').contents());
-    	if($('iframe').contents().find('a').length) {
-    		console.log('found the element');
-    	}
-    	else {
-    		console.log('cannot found the element');	
-    	}
-    	$('#scaled-frame').contents().find('a[href="/login"]').trigger('click');
-	});
+    $("iframe#scaled-frame").load(function () {
+        var $content = $("iframe").contents();
+        var $element = $content.find('a[href="/login"]');
+        if($element.length) {
+            //$element.get(0).click();
+        }
 
+        
+    });
     
+    $('#btn').click(function(){
+        console.log("1");
+        $("iframe#scaled-frame").contents().find('#btn-login').get(0).click();
+            console.log("2");
+
+        $("iframe#scaled-frame").one( "load", function() { 
+            console.log("3");
+            $("iframe#scaled-frame").contents().find('#email').val('valid@email.com');
+            console.log("4");
+            $("iframe#scaled-frame").contents().find('#pwd').val('secret');
+            console.log("5");
+            $("iframe#scaled-frame").contents().find('#submit-btn').get(0).click();
+            
+            $("iframe#scaled-frame").one( "load", function() { 
+                console.log("6");
+                console.log($("iframe#scaled-frame").contents().find('.login-welcome-msg').text().should.equal('Your login is successful'));
+            });
+        });
+    });
     
 });
